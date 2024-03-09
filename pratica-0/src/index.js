69bc4579cb942b168b741e90e50f71cff756997a
João Pedro Andrade Moreira <moreira.joao@gec.inatel.br>
2024-03-09 14:41:02 -0300

    Pratica-0

---
+++ pratica-0/src/index.js
@@ -0,0 +1,9 @@
+// Exemplo de uso prático
+let nome = "Joao Pedro Andrade Moreira - 1723 ";
+function saudacaoPersonalizada(nome) {
+  console.log("Olá, " + nome + "!");
+}
+saudacaoPersonalizada(nome);
+
+let modulo = require("./module.js");
+modulo.saudacao();
---
+++ pratica-0/src/module.js
@@ -0,0 +1,7 @@
+// arquivo modulo.js
+module.exports = {
+  mensagem: "Testando modulo: Joao Pedro Andrade Moreira - 1723 !",
+  saudacao: function () {
+    console.log(this.mensagem);
+  },
+};
